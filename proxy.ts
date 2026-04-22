d429dbd5933796ccf91426f833a4bf1b25b57cdc
terak <tera.kyo0511@gmail.com>
2026-04-22 18:19:23 +0900

    fix proxy routing

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,7 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-const handler = createMiddleware(routing);
-
-export function proxy(request: Request) {
-  return handler(request as Parameters<typeof handler>[0]);
-}
+export const proxy = createMiddleware(routing);
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
